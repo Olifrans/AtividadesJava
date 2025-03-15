d34e371eccd5d7c5c5c1a7120026e5f700390e41
Francisco Oliveira <fransoliveira@gmail.com>
2025-03-15 10:41:43 -0300

    feat: Atividades java

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    //Média de Três Números
+        int num01 = 77;
+        int num02 = 11;
+        int num03 = 27;
+        int media = (num01+num02+num03)/3;
+        double media01 = (num01+num02+num03)/3.0;
+        float media02 = (num01+num02+num03)/3.0f;
+        long media03 = (num01+num02+num03)/3L;
+
+        System.out.println("A média do inteiro é: "+ media);
+        System.out.println("A média do número real é: "+ media01);
+        System.out.println("A média do número float é: "+ media02);
+        System.out.println("A média do número long é: "+ media03);
+
+    }
+}
